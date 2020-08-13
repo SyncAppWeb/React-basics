2edbf826e508154b584cbe46bc8bfa75d77a8055
root <root@Niko.Niko.net>
2020-08-13 15:49:06 -0500

    react basic estructure

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          steven fernandez 
         </a>
       </header>
     </div>
